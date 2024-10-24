55a05a9ee10b82f2c4c94788c6b4bd4ee11f1e42
Vishal Gidwani <vishal.0694@gmail.com>
2024-10-24 16:13:38 +0530

    added grid

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 import './style.css'
 
-document.querySelector('#root').innerHTML = `
-Hello
-`
+function setupGrid() {
+    const grid = document.createElement("div")
+    grid.className = "size-[40rem] flex flex-col justify-evenly"
+    document.querySelector("#root").appendChild(grid)
+
+
+    for (let r = 0; r < 15; r++) {
+        const row = document.createElement("div")
+        row.id = `row-1${r}`
+        row.className = "flex justify-evenly"
+        grid.appendChild(row)
+
+        for (let c = 0; c < 15; c++) {
+            const cell = document.createElement("div")
+            cell.id = `column-1${r}`
+            cell.innerHTML = `${r}-${c}`
+            row.appendChild(cell)
+        }
+    }
+}
+
+setupGrid()
