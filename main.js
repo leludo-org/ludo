9cf8585f5226368b58363bcf395c524ab0cb2756
Vishal Gidwani <vishal.0694@gmail.com>
2024-10-24 16:43:01 +0530

    grid changes

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 import './style.css'
 
+const PLAYER_1_COLOR = "bg-red-500"
+const PLAYER_2_COLOR = "bg-green-500"
+const PLAYER_3_COLOR = "bg-yellow-500"
+const PLAYER_4_COLOR = "bg-blue-500"
+
+// todo: need to numerical color instead of this ugly array, just used to visualize for now
+const GRID_CELL_STYLES = [
+    [PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, "", "", "", PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR],
+    [PLAYER_1_COLOR, "", "", "", "", PLAYER_1_COLOR, "", "", "", PLAYER_2_COLOR, "", "", "", "", PLAYER_2_COLOR],
+    [PLAYER_1_COLOR, "", "", "", "", PLAYER_1_COLOR, "", "", "", PLAYER_2_COLOR, "", "", "", "", PLAYER_2_COLOR],
+    [PLAYER_1_COLOR, "", "", "", "", PLAYER_1_COLOR, "", "", "", PLAYER_2_COLOR, "", "", "", "", PLAYER_2_COLOR],
+    [PLAYER_1_COLOR, "", "", "", "", PLAYER_1_COLOR, "", "", "", PLAYER_2_COLOR, "", "", "", "", PLAYER_2_COLOR],
+    [PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, PLAYER_1_COLOR, "", "", "", PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR, PLAYER_2_COLOR],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    [PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, "", "", "", PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR],
+    [PLAYER_4_COLOR, "", "", "", "", PLAYER_4_COLOR, "", "", "", PLAYER_3_COLOR, "", "", "", "", PLAYER_3_COLOR],
+    [PLAYER_4_COLOR, "", "", "", "", PLAYER_4_COLOR, "", "", "", PLAYER_3_COLOR, "", "", "", "", PLAYER_3_COLOR],
+    [PLAYER_4_COLOR, "", "", "", "", PLAYER_4_COLOR, "", "", "", PLAYER_3_COLOR, "", "", "", "", PLAYER_3_COLOR],
+    [PLAYER_4_COLOR, "", "", "", "", PLAYER_4_COLOR, "", "", "", PLAYER_3_COLOR, "", "", "", "", PLAYER_3_COLOR],
+    [PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, PLAYER_4_COLOR, "", "", "", PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR, PLAYER_3_COLOR],
+]
+
 function setupGrid() {
     const grid = document.createElement("div")
     grid.className = "size-[40rem] grid grid-cols-15"
@@ -9,7 +33,7 @@ function setupGrid() {
         for (let c = 0; c < 15; c++) {
             const cell = document.createElement("div")
             cell.id = `cell-${r}-${c}`
-            cell.className = "flex items-center justify-center bg-sky-100 border border-gray-500"
+            cell.className = "flex items-center justify-center bg-sky-100 border border-black " + GRID_CELL_STYLES[r][c]
             grid.appendChild(cell)
         }
     }
