7aefadfaa92c15f2a2067371dcffc98c374d0db7
Vishal Gidwani <vishal.0694@gmail.com>
2024-10-25 13:28:09 +0530

    theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-  ],
-  theme: {
-    extend: {
+    content: [
+        "./*.{html,js}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "background": "hsl(var(--background))",
+                "player-1": "hsl(var(--player-1))",
+                "player-2": "hsl(var(--player-2))",
+                "player-3": "hsl(var(--player-3))",
+                "player-4": "hsl(var(--player-4))"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
