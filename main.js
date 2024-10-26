bc2ca68f990f56050ab41c3b20c0d95cbd89e6e6
Vishal Gidwani <vishal.0694@gmail.com>
2024-10-26 13:07:13 +0530

    move fixes

--- main.js
+++ main.js
@@ -1,45 +1,63 @@
 import './style.css' // todo: check if better way to import style
 
-const positions = new Array(15).fill(-1);
+const positions = new Array(15).fill(-1)
 
 
 document.addEventListener("DOMContentLoaded", () => {
     setInitialPosition()
 
-    const testButton = document.getElementById("test");
+    const testButton = document.getElementById("test")
     testButton.addEventListener("click", () => {
-        movePiece(0);
-    });
-});
+        positions[4] += 4
+        movePiece(4)
+    })
+})
 
 function setInitialPosition() {
-    const params = new URLSearchParams(window.location.search);
+    const params = new URLSearchParams(window.location.search)
     params.get("positions")
         ?.split(",")
-        .forEach(((p, index) => {
-            positions[index] = +p
-            movePiece(index)
+        .forEach(((position, pieceIndex) => {
+            positions[pieceIndex] = +position
+            movePiece(pieceIndex)
         }))
 }
 
 /**
  *
- * @param {number} piece
+ * @param {number} pieceIndex
  */
-function movePiece(piece) {
-    const pieceElement = document.getElementById(`p${piece}`);
-    const initialPosition = pieceElement.getBoundingClientRect();
+function movePiece(pieceIndex) {
+    const pieceElement = document.getElementById(`p${pieceIndex}`)
+    const initialPosition = pieceElement.getBoundingClientRect()
 
-    const targetContainer = document.getElementById("m0");
-    const finalPosition = targetContainer.getBoundingClientRect();
+    const targetContainerId = findTargetContainerId(pieceIndex)
+    const targetContainer = document.getElementById(targetContainerId)
+    const finalPosition = targetContainer.getBoundingClientRect()
 
-    const offsetX = finalPosition.left - initialPosition.left;
-    const offsetY = finalPosition.top - initialPosition.top;
+    const offsetX = finalPosition.left - initialPosition.left
+    const offsetY = finalPosition.top - initialPosition.top
 
-    pieceElement.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    pieceElement.style.transform = `translate(${offsetX}px, ${offsetY}px)`
     setTimeout(() => {
-        pieceElement.style.transform = `translate(0px, 0px)`;
-        targetContainer.appendChild(pieceElement);
-        z
+        pieceElement.style.transform = `translate(0px, 0px)`
+        targetContainer.appendChild(pieceElement)
     }, 1000)
+}
+
+/**
+ *
+ * @param {number} pieceIndex
+ * @return {string}
+ */
+function findTargetContainerId(pieceIndex) {
+    const piecePosition = positions[pieceIndex]
+    if (piecePosition === -1) {
+        return `h${pieceIndex}`
+    }
+
+    const playerIndex = Math.floor(pieceIndex / 4)
+    const markIndex = (piecePosition + (13 * playerIndex)) % 52
+
+    return `m${markIndex}`
 }
