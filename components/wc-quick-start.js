a29491bf962b0d19c60c91798ebfbf92e3b2d82f
Vishal Gidwani <vishal.0694@gmail.com>
2024-11-10 14:08:23 +0530

    home visibility fix

--- components/wc-quick-start.js
+++ components/wc-quick-start.js
@@ -163,7 +163,7 @@ const QUICK_START_HTML = `
                 </svg>
             </label>
 
-            <button class="flex-1 rounded p-2 bg-white shadow hover:shadow-2xl hover:bg-gray-100">
+            <button id="mm-start" class="flex-1 rounded p-2 bg-white shadow hover:shadow-2xl hover:bg-gray-100">
                 0
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
