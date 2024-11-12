01e7eb91f20e10e7385a43dafe9e126277301840
Vishal Gidwani <vishal.0694@gmail.com>
2024-11-12 18:47:09 +0530

    refactoring

--- test/test.js
+++ test/test.js
@@ -9,10 +9,14 @@
  * @property {function} equal
  */
 
-import {isTokenMovable} from "../game-logic.js";
+import {isTokenMovable, isUnsafePosition} from "../game-logic.js";
 
 QUnit.module('game-logic', function () {
     QUnit.test('is token movable - home, less than 6', function (assert) {
         assert.equal(isTokenMovable(-1, 5), false);
+    });
+
+    QUnit.test('is unsafe position - 7', function (assert) {
+        assert.equal(isUnsafePosition(7), true);
     });
 });
