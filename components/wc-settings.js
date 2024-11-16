0458db934370650c736d35fdd1a5a4510c15fc57
Vishal Gidwani <vishal.0694@gmail.com>
2024-11-16 09:28:01 +0530

    adding comment to support html formatting in vs code for WC

--- components/wc-settings.js
+++ components/wc-settings.js
@@ -1,26 +1,26 @@
-import {htmlToElement} from "../utils.js"
+import {
+    htmlToElement
+} from "../utils.js"
 
 //language=HTML
-const SETTINGS_HTML = `
-    <button id="settings-icon" class="flex items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-             class="size-6">
-            <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/>
-            <circle cx="12" cy="12" r="3"/>
-        </svg>
-    </button>
-    <div id="settings-container" class="fixed flex flex-col bg-card p-2 gap-2 right-8 top-12 w-48 hidden">
-        <h1>Settings</h1>
-        <div class="text-sm flex justify-between">
-            <label for="s-theme">Theme</label>
-            <select id="s-theme" class="bg-background">
-                <option value="system">System</option>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-            </select>
-        </div>
+const SETTINGS_HTML = /*html*/ `
+<button id="settings-icon" class="flex items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-6">
+        <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+        <circle cx="12" cy="12" r="3" />
+    </svg>
+</button>
+<div id="settings-container" class="fixed flex flex-col bg-card p-2 gap-2 right-8 top-12 w-48 hidden">
+    <h1>Settings</h1>
+    <div class="text-sm flex justify-between">
+        <label for="s-theme">Theme</label>
+        <select id="s-theme" class="bg-background">
+            <option value="system">System</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
     </div>
+</div>
 `
 
 /**
@@ -32,9 +32,9 @@ function updateTheme(theme) {
     rootElement.classList.remove("dark", "light", "system")
 
     // note: didn't subscribe to watch for system change yet...
-    const themeToApply = theme === "system"
-        ? (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
-        : theme;
+    const themeToApply = theme === "system" ?
+        (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light") :
+        theme;
     rootElement.classList.add(themeToApply)
 
     const navElementStyles = getComputedStyle(rootElement.querySelector("#header"));
